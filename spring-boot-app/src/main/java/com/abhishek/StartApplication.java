47ef394c74c6e9e868148b79b5f5b6fedf1d9311
mayur <msbhoge435@gmail.com>
2024-03-15 21:11:08 +0530

    new commit

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
+        model.addAttribute("title", "I have successfuly built a spring boot application using Maven and I am Mayur !!!");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
